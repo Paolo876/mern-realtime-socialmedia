d62a51237b49c04fafc7043ce7873a44b23fd9e6
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-04 01:03:36 -0800

    updated dependencies, updated connectDb file

--- utilsServer/connectDb.js
+++ utilsServer/connectDb.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
-async function connectDb() {
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
-    console.log("Mongodb connected");
+    // await mongoose.connect(process.env.MONGO_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false
+    // });
+    // console.log("Mongodb connected");
+    const db = await mongoose.connect(process.env.MONGO_URI)
+    mongoose.set('bufferCommands', false);
+    console.log("mongodb connected: ".cyan.underline.bold, db.connection.host)
   } catch (error) {
-    console.log(error);
+    console.log("error: ".red.underline.bold, error.message)
     process.exit(1);
   }
 }
